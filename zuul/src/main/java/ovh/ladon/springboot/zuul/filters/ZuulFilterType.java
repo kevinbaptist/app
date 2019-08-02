38417cbebbdb6cdc7f9c8d1520e7e7b1eba8c4e2
Kevin Baptista <kevin.baptista@isobar.com>
2019-08-02 16:29:06 +0100

    added comment

--- zuul/src/main/java/ovh/ladon/springboot/zuul/filters/ZuulFilterType.java
+++ zuul/src/main/java/ovh/ladon/springboot/zuul/filters/ZuulFilterType.java
@@ -1,5 +1,12 @@
 package ovh.ladon.springboot.zuul.filters;
 
+/**
+ * Zuul has four standard filters:
+ * -> pre filters are executed before the request is routed,
+ * -> route filters can handle the actual routing of the request,
+ * -> post filters are executed after the request has been routed, and
+ * -> error filters execute if an error occurs in the course of handling the request.
+ */
 public enum ZuulFilterType {
 	PRE, ROUTE, POST, ERROR;
 
